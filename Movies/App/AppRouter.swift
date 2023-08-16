6fa5faa28ff791c8725885bb911e2c986f4c301d
Erdem Papakci <erdmppkc@gmail.com>
2023-08-16 21:09:26 +0300

    scene delegate conf

--- Movies/App/AppRouter.swift
+++ Movies/App/AppRouter.swift
@@ -5,4 +5,23 @@
 //  Created by Erdem on 16.08.2023.
 //
 
-import Foundation
+import UIKit
+
+final class AppRouter {
+    static let shared = AppRouter() //
+    var windowScene: UIWindowScene?
+    var window: UIWindow?
+    
+    private init() {}
+    
+    func configureWindow(windowScene: UIWindowScene) {
+        self.windowScene = windowScene
+        self.window = UIWindow(windowScene: windowScene)
+        
+       
+        let rootViewController = TabBarBuilder.build()
+        window?.rootViewController = rootViewController
+        
+        window?.makeKeyAndVisible()
+    }
+}
--- Movies/SceneDelegate.swift
+++ Movies/SceneDelegate.swift
@@ -12,13 +12,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = TabBarBuilder.build()
-           window?.makeKeyAndVisible()
-        window?.windowScene = windowScene
-        
+         
+               AppRouter.shared.configureWindow(windowScene: windowScene)
     }
 
 
