c5628394fb63fc87a92b5cdfaff5e19cfbe67f50
Erdem Papakci <erdmppkc@gmail.com>
2023-08-26 17:49:29 +0300

    + collectionview animations

--- Movies/Sceens/Main/MainCollectionViewAdapter.swift
+++ Movies/Sceens/Main/MainCollectionViewAdapter.swift
@@ -12,6 +12,9 @@ class MainCollectionViewAdapter: NSObject, UIGestureRecognizerDelegate{
     private let presenter: MainPresenterProtocol
     private let view: MainViewProtocol
     
+    let animationDuration: Double = 0.3
+    let delayBase: Double = 0.3
+    
     init(presenter: MainPresenterProtocol, view: MainViewProtocol) {
         self.presenter = presenter
         self.view = view
@@ -28,12 +31,25 @@ extension MainCollectionViewAdapter: UICollectionViewDelegate, UICollectionViewD
    
   
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+    
         let cell = collectionView.genericdequeueReusableCell(type: MovieListCell.self, indexPath)
-        
+        cell.alpha = 0
         cell.backgroundColor = .white
-        cell.configure(presenter.cellForRowAt(index: indexPath.row))
-      
+        
+        cell.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+       
+        let column = Double(cell.frame.minX / cell.frame.width)
+        let row = Double(cell.frame.minY / cell.frame.height)
+        let distance = sqrt(pow(column, 2) + pow(row, 2))
+        let delay = sqrt(distance) * delayBase
+        UIView.animate(withDuration: animationDuration, delay: delay, usingSpringWithDamping: 0.5, initialSpringVelocity: 10, options: [], animations: { [weak self] in
+            
+            guard let self = self else {return}
+          cell.alpha = 1
+          cell.transform = .identity
+            cell.configure(self.presenter.cellForRowAt(index: indexPath.row))
+        })
+        
         return cell
     }
   
@@ -41,5 +57,5 @@ extension MainCollectionViewAdapter: UICollectionViewDelegate, UICollectionViewD
         collectionView.deselectItem(at: indexPath, animated: false)
         presenter.routeToDetail(id:  presenter.didSelectItem(indexPath.row))
     }
- 
+   
 }
